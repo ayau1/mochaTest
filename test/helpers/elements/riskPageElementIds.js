471beea9ec5284a3e8c85c661f1f350050d1c851
ayau1 <amie.yau@bglgroup.co.uk>
2017-10-30 19:43:55 +0000

    added more to the complete single risk method

--- test/helpers/elements/riskPageElementIds.js
+++ test/helpers/elements/riskPageElementIds.js
@@ -1,4 +1,15 @@
 //I need to export the objects
 module.exports = {
-       proposerTitle: "#proposerTitle"
+        proposerTitle: "#proposerTitle",
+        proposerFirstName: "#proposerFirstname",
+        proposerSurname: "#proposerSurname",
+        proposerDateOfBirthDay: "#proposerDateOfBirthDay",
+        proposerDateOfBirthMonth: "#proposerDateOfBirthMonth",
+        proposerDateOfBirthYear: "#proposerDateOfBirthYear",
+        proposerNonSmokerLabel: "#proposerNonSmokerLabel",
+        yearsTextBox: "#yearsTextBox",
+        amount: "#amount",
+        proposerEmailAddress: "#email",
+        proposerHouseNumber: "#houseNumber",
+        proposerPostcode: "#postcode"
     };
--- test/helpers/methods/completeValidSingleRisk.js
+++ test/helpers/methods/completeValidSingleRisk.js
@@ -3,5 +3,17 @@ const riskPageElementIds = require('../elements/riskPageElementIds');
 
 module.exports = function() {
 
-    browser.click(riskPageElementIds.proposerTitle);
+    browser.click(riskPageElementIds.proposerTitle)
+        .selectByValue(riskPageElementIds.proposerTitle,"MRS")
+        .setValue(riskPageElementIds.proposerFirstName,"Test")
+        .setValue(riskPageElementIds.proposerSurname,"Test")
+        .selectByValue(riskPageElementIds.proposerDateOfBirthDay,"string:01")
+        .selectByValue(riskPageElementIds.proposerDateOfBirthMonth,"string:03")
+        .selectByValue(riskPageElementIds.proposerDateOfBirthYear,"number:1979")
+        .click(riskPageElementIds.proposerNonSmokerLabel)
+        .setValue(riskPageElementIds.yearsTextBox,"25")
+        .setValue(riskPageElementIds.amount,"250000")
+        .setValue(riskPageElementIds.proposerEmailAddress,"test@test.com")
+        .setValue(riskPageElementIds.proposerHouseNumber,"1")
+        .setValue(riskPageElementIds.proposerPostcode,"PE2 6XJ")
 }
