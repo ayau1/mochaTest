0e2a80b43a0497636b2003a6dad6c91459ad1e84
ayau1 <amie.yau@bglgroup.co.uk>
2017-12-18 09:47:56 +0000

    added comments to test.js and changed the spec path i wdioPROD.conf.js

--- test/specs/test.js
+++ test/specs/test.js
@@ -5,6 +5,10 @@
 // to run test type wdio wdioPROD.conf.js from the mochatest level. Selenium server, webdriverio and wdio-mocha-framework
 //and wdio-selenium-stanalone-service are all installed globally
 //To run a single test from a test file in cmd e.g. wdio --mochaOpts.grep "I should be able to apply online" wdioLOCAL.conf.js
+
+//or if you do not have them installed globally, then navigate to the node_modules/.bin first and change youe spec file path accordingly in wdio config
+// e.g. wdio ../../wdioPROD.conf.js
+// and your spec file path in the conf should be in this instance '../../test/specs/test.js'
 "use strict"
 
 // I need to require a hooks file for each spec run.[y
